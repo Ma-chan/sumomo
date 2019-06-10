38462857e032341cf84a493912c270998c4b53f3
松本彩花 <matsumotoayaka@PoohGirl.local>
2019-06-10 17:24:17 +0900

    add megane

--- sumomo/front/src/editor.ts
+++ sumomo/front/src/editor.ts
@@ -42,10 +42,16 @@ let x = 100;
 
 document.getElementById('zoom-in').addEventListener('click', () => {
   { x *= 1.2; document.getElementById('editor').style.fontSize = x + '%'; }
-
 });
 
 document.getElementById('zoom-out').addEventListener('click', () => {
   { x /= 1.2; document.getElementById('editor').style.fontSize = x + '%'; }
 
+});
+$('.tab_label').on('click', function () {
+  const $th = $(this).index();
+  $('.tab_label').removeClass('active');
+  $('.tab_panel').removeClass('active');
+  $(this).addClass('active');
+  $('.tab_panel').eq($th).addClass('active');
 });
